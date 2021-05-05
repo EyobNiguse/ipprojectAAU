1c91319534c662f0e8a6df143d60f2e2ac0d44f2
eyob ng <eyobnigusework@gmail.com>
2021-05-05 10:51:59 -0400

    basic theme

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+const navigationHeader = document.getElementById("navigation");
+button.addEventListener('click',()=>{
+console.log("working");
+})
