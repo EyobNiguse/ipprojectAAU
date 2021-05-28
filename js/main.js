78d8e07f69a2755b12f51b6fabc01ca0d191f4eb
EyobNiguse <eyobnigusework@gmail.com>
2021-05-28 16:40:12 -0400

    landing content Done

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ button.addEventListener('click',()=>{
 
     if(hidden){
         navigationHeader.classList.toggle('menu-header');
+        console.log(document.getElementsByClassName("hidden-menu")[0].style.display);
+        if(document.getElementsByClassName("hidden-menu")[0].style.display === 'block'){
+            document.getElementsByClassName("hidden-menu")[0].style.display =  "none"; 
+        }else{
+            document.getElementsByClassName("hidden-menu")[0].style.display =  "block"; 
+        }
+            
     }})
