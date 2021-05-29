0312471137643c2822b6d9ba4a98d71351b11738
EyobNiguse <eyobnigusework@gmail.com>
2021-05-29 09:38:51 -0400

    login Ui feature added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const button = document.getElementById("button");
 const navigationHeader = document.getElementById("navigation");
 var hidden = true;
+const login =  document.getElementById("login");
+const form = document.getElementById("main-form");
+const submit = document.getElementById("submit-btn");
+const confirm =  document.getElementById("pass-confirm");
+const usr =  document.getElementById("username");
+const notice = document.getElementById("notice-form");
 button.addEventListener('click',()=>{
 
     if(hidden){
@@ -20,3 +26,11 @@ button.addEventListener('click',()=>{
         }
             
     }})
+    login.addEventListener('click', ()=>{
+     form.setAttribute("action","login.php");
+     submit.innerHTML = 'Login';
+     notice.innerHTML = "Login";
+     confirm.remove();
+     usr.remove();
+     location.replace("#content");
+    });
